f06a7cbfcbe6c12a9d5c72c893441fbbbcf9f55a
kaquin0 <66040819+kaquin0@users.noreply.github.com>
2020-08-11 23:17:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 ad_clicks = pd.read_csv('ad_clicks.csv')
 print(ad_clicks.head(10))
-ad_clicks.groupby('utm_source')\
+print(ad_clicks.groupby('utm_source')\
 		.user_id.count()\
-		.reset_index()
+		.reset_index())
 
 ad_clicks['is_click'] = ~ad_clicks\
    .ad_click_timestamp.isnull()
@@ -20,20 +20,24 @@ clicks_pivot = clicks_by_source.pivot(
   index = 'utm_source',
   values = 'user_id').reset_index()
 
+clicks_pivot['percent_clicked'] = clicks_pivot[True] / (clicks_pivot[True] + clicks_pivot[False])
 
-
-clicks_pivot['percent_clicked'] = clicks_pivot[True] / clicks_pivot[True] + clicks_pivot[False]
+print(clicks_pivot)
 
 group_count = ad_clicks\
   	.groupby('experimental_group').user_id\
        .count()\
        .reset_index()
 
+print(group_count)
+
 greater_percentage = ad_clicks.groupby(['experimental_group', 'is_click']).user_id.count().reset_index().pivot(
 		index = 'experimental_group',
 		columns = 'is_click',
 		values = 'user_id'
 ).reset_index()
+
+print(greater_percentage)
 
 a_clicks = ad_clicks[
    ad_clicks.experimental_group
@@ -59,6 +63,15 @@ b_clicks_pivot = b_clicks.groupby(['is_click', 'day']).user_id.count().reset_ind
 		values = 'user_id'
 ).reset_index()
 
-b_percentage = b_clicks_pivot[True] / (b_clicks_pivot[True] + b_clicks_pivot[False])
+b_precentage = b_clicks_pivot[True] / (b_clicks_pivot[True] + b_clicks_pivot[False])
 
-print(b_percentage)
+print(b_precentage)
+
+
+
+
+
+
+
+
+
